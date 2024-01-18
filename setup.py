09abf10cab8e6d5f083f4546538b0849665dc069
Martok <martok@martoks-place.de>
2024-01-18 14:01:09 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mplotkit",
-    version="0.2.0",
+    version="0.3.0",
     author="Martok",
     author_email="martok@martoks-place.de",
     description="Collection of helpers for plotting with matplotlib",
